6905c2526bcbad5c1938d68ae8418091ebaa47ca
Linus\Leon <javascouts@gmail.com>
2020-02-12 20:35:58 -0500

    add committs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_B1_New.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_B1_New.java
@@ -5,6 +5,12 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 @Autonomous(name = "Auto B1")
 public class Auto_B1_New extends BaseTrajectory {
 
+    // TO EDIT SERVO POSITIONINGS AND TIMINGS, SEE LINES 214 - 236 in BaseTrajectory
+    // To EDIT others its pretty self-explanatory
+    // if the ROBOT does not correct its ANGLE fast ENOUGH, increase KP by 3 or 4 until it does (BaseTrajectory)
+    // if results are not expected, call leon
+    // or use band
+
     @Override
     public void afterRun() {
 
@@ -12,6 +18,7 @@ public class Auto_B1_New extends BaseTrajectory {
         switch(stone) {
 
             case 1:
+                //move to 1, grab and raise, move to found
                 moveRelative(4, 36.2,0, 2);
                 grab_stone();
                 raise_stone();
@@ -20,6 +27,7 @@ public class Auto_B1_New extends BaseTrajectory {
                 break;
 
             case 2:
+                //move to 2, grab and raise, move to found
                 moveRelative(-3, 36.2,0, 2);
                 grab_stone();
                 raise_stone();
@@ -28,6 +36,7 @@ public class Auto_B1_New extends BaseTrajectory {
                 break;
 
             case 3:
+                //move to 3, grab and raise, move to found
                 moveRelative(-11, 36.2,0, 2.01);
                 grab_stone();
                 raise_stone();
@@ -44,13 +53,14 @@ public class Auto_B1_New extends BaseTrajectory {
                 break;
         }
 
+        //strafe into it and place and strafe out
         moveRelative(0, 15, 0, 0.5);
         drop_stone();
         moveRelative(0,-15,0,0.5);
 
         switch(stone) {
 
-            case 1:
+            case 1: //case 4 (same as case 1 but move back instead of moving out)
                 moveRelative(-60, 5, 0, 3);
                 moveRelative(0, 10, 0, 1);
                 grab_stone();
@@ -58,7 +68,7 @@ public class Auto_B1_New extends BaseTrajectory {
                 moveRelative(54, 0, 0, 3);
                 break;
 
-            case 2:
+            case 2: //case 5
                 moveRelative(-66, 5, 0, 3);
                 moveRelative(0, 10, 0, 0.5);
                 grab_stone();
@@ -66,7 +76,7 @@ public class Auto_B1_New extends BaseTrajectory {
                 moveRelative(60, 0, 0, 3);
                 break;
 
-            case 3:
+            case 3: //cae 6
                 moveRelative(-72, 5, 0, 3);
                 moveRelative(0, 10, 0, 0.5);
                 grab_stone();
